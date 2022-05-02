96461887dbd575172f89fcc542e180e9f8b6b473
jake.liu <jake.liu@baozun.com>
2022-05-02 16:41:39 +0800

    理解什么是reducer,他源自redux

--- src/ReducerTutorial/ReducerTutorialV2.js
+++ src/ReducerTutorial/ReducerTutorialV2.js
@@ -1,6 +1,20 @@
 import { useReducer, useState } from "react";
 
 // 定义reducer行为
+// 这样的函数称之为reducer，是因为这种函数与被传入 
+// Array.prototype.reduce(reducer, ?initialValue) 的回调函数属于相同的类型。
+/**
+ * https://blog.logrocket.com/react-usereducer-hook-ultimate-guide/#:~:text=useReducer%20returns%20an%20array%20that,coupled%20to%20a%20specific%20reducer.
+ * useReducer 是 React v16.8 附带的附加 Hook 之一。
+ * 作为 useState Hook 的替代方案，useReducer 
+ * 可帮助您管理 React 应用程序中的复杂状态逻辑。
+ * 当与 useContext 等其他 Hook 结合使用时，
+ * useReducer 可以很好地替代 Redux、Recoil 或 MobX。
+ * 在某些情况下，这是一个更好的选择。
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
 const reducer = (state,action)=>{
     switch(action.type){
         case "INCREMENT":
